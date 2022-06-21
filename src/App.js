7024967e7a6949d45cea9ce6ad41bd6de5c97e14
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-06-21 23:53:21 +0300

    improve footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './assets/css/main.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing/Landing'
 import Navbar from './components/shared/Navbar'
+import Footer from './components/shared/Footer'
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Routes>
                     <Route exact path="/" element={<Landing />} />
                 </Routes>
+                <Footer/>
             </BrowserRouter>
         </>
     )
---
+++ src/components/shared/Footer.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className="fixed bottom-0 w-full py-6">
+            <div className="container mx-auto bottom-0">
+                <div className="flex justify-center mb-1">
+                    <a
+                        href="https://www.instagram.com/rixusart/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className='mx-2'
+                    >
+                        <svg role="img" className="w-9 h-9 text-primary-black">
+                            <use xlinkHref="/assets/icons/sprite.svg#instagram" />
+                        </svg>
+                    </a>
+
+                    <a
+                        href="https://www.facebook.com/rixusart"
+                        target="_blank"
+                        rel="noreferrer"
+                        className='mx-2'
+                    >
+                        <svg role="img" className="w-9 h-9 text-primary-black">
+                            <use xlinkHref="/assets/icons/sprite.svg#facebook" />
+                        </svg>
+                    </a>
+
+                    <a
+                        href="https://tr.pinterest.com/rixusart/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className='mx-2'
+                    >
+                        <svg role="img" className="w-9 h-9 text-primary-black">
+                            <use xlinkHref="/assets/icons/sprite.svg#pinterest" />
+                        </svg>
+                    </a>
+                </div>
+                <div className="text-center w-full">
+                    ©2022 by rixusart Ankara, Turkey
+                </div>
+            </div>
+        </div>
+    )
+}
