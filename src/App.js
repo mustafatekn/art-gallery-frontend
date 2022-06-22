65aac8c030289ee8e011e2fd37da9deb54784a13
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-06-23 00:55:56 +0300

    remove footer and navbar, create DefaultLayout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './assets/css/main.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing/Landing'
-import Navbar from './components/shared/Navbar'
-import Footer from './components/shared/Footer'
 
 function App() {
     return (
         <>
             <BrowserRouter>
-                <Navbar />
                 <Routes>
                     <Route exact path="/" element={<Landing />} />
                 </Routes>
-                <Footer/>
             </BrowserRouter>
         </>
     )
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,14 +1,15 @@
-// import React from 'react'
-// import Header from './components/Header'
+import DefaultLayout from '../../layouts/DefaultLayout'
 
 export default function Landing() {
     return (
-        <div className="container mx-auto">
-            <img
-                src="/assets/images/landing-cat.jpg"
-                className="mx-auto w-1/2 max-w-screen mt-12"
-                alt="rixusart-cat"
-            />
-        </div>
+        <DefaultLayout>
+            <div className="container mx-auto">
+                <img
+                    src="/assets/images/landing-cat.jpg"
+                    className="mx-auto w-1/2 max-w-screen mt-12"
+                    alt="rixusart-cat"
+                />
+            </div>
+        </DefaultLayout>
     )
 }
