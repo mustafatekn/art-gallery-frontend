5a5285a1f72211b739c0eeae01588432de837607
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-07-07 01:03:15 +0300

    replace mb-28 with mb-20 on default layout

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -8,41 +8,101 @@ export default function Projects() {
             id: 1,
             title: 'title 1',
             text: 'text 1',
-            imageUrl:
-                'https://www.thoughtco.com/thmb/VG5-1LGCmdWyW23Uf7Vr-R_5tqk=/3504x2336/filters:fill(auto,1)/GettyImages-116248249-5be59c3cc9e77c00514fed87.jpg',
-            url: 'black',
+            images: [
+                {
+                    url: 'https://www.thoughtco.com/thmb/VG5-1LGCmdWyW23Uf7Vr-R_5tqk=/3504x2336/filters:fill(auto,1)/GettyImages-116248249-5be59c3cc9e77c00514fed87.jpg',
+                    explanation: 'link',
+                },
+                {
+                    url: 'https://static.remove.bg/remove-bg-web/5c20d2ecc9ddb1b6c85540a333ec65e2c616dbbd/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg',
+                    alt: 'girl',
+                },
+                {
+                    url: 'https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg',
+                    explanation: 'profile-image',
+                },
+            ],
+            thumbnail: {
+                url: 'https://static.remove.bg/remove-bg-web/5c20d2ecc9ddb1b6c85540a333ec65e2c616dbbd/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg',
+                explanation: 'girl',
+            },
+            url: 'first-project',
         },
         {
-            id: 1,
+            id: 2,
             title: 'title 2',
             text: 'text 2',
-            imageUrl:
-                'https://static.remove.bg/remove-bg-web/5c20d2ecc9ddb1b6c85540a333ec65e2c616dbbd/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg',
-            url: 'girl',
+            images: [
+                {
+                    url: 'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
+                    explanation: 'butterfly',
+                },
+                {
+                    url: 'https://static.remove.bg/remove-bg-web/5c20d2ecc9ddb1b6c85540a333ec65e2c616dbbd/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg',
+                    alt: 'girl',
+                },
+                {
+                    url: 'https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg',
+                    explanation: 'profile-image',
+                },
+            ],
+            thumbnail: {
+                url: 'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
+                explanation: 'butterfly',
+            },
+            url: 'second-project',
         },
         {
-            id: 1,
+            id: 3,
             title: 'title 3',
             text: 'text 3',
-            imageUrl:
-                'https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg',
-            url: 'profile-image',
+            images: [
+                {
+                    url: 'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
+                    explanation: 'butterfly',
+                },
+                {
+                    url: 'https://static.remove.bg/remove-bg-web/5c20d2ecc9ddb1b6c85540a333ec65e2c616dbbd/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg',
+                    alt: 'girl',
+                },
+                {
+                    url: 'https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg',
+                    explanation: 'profile-image',
+                },
+            ],
+            thumbnail: {
+                url: 'https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg',
+                explanation: 'profile-image',
+            },
+            url: 'third-project',
         },
         {
-            id: 1,
+            id: 4,
             title: 'title 4',
             text: 'text 4',
-            imageUrl:
-                'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
-            url: 'butterfly',
-        },
-        {
-            id: 1,
-            title: 'title 5',
-            text: 'text 5',
-            imageUrl:
-                'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300',
-            url: 'camera',
+            images: [
+                {
+                    url: 'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg',
+                    explanation: 'butterfly',
+                },
+                {
+                    url: 'https://static.remove.bg/remove-bg-web/5c20d2ecc9ddb1b6c85540a333ec65e2c616dbbd/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg',
+                    alt: 'girl',
+                },
+                {
+                    url: 'https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg',
+                    explanation: 'profile-image',
+                },
+                {
+                    url: 'https://www.thoughtco.com/thmb/VG5-1LGCmdWyW23Uf7Vr-R_5tqk=/3504x2336/filters:fill(auto,1)/GettyImages-116248249-5be59c3cc9e77c00514fed87.jpg',
+                    explanation: 'link',
+                },
+            ],
+            thumbnail: {
+                url: 'https://www.thoughtco.com/thmb/VG5-1LGCmdWyW23Uf7Vr-R_5tqk=/3504x2336/filters:fill(auto,1)/GettyImages-116248249-5be59c3cc9e77c00514fed87.jpg',
+                explanation: 'link',
+            },
+            url: 'fourth-project',
         },
     ])
 
@@ -51,10 +111,12 @@ export default function Projects() {
             <div className="container flex flex-col mx-auto text-center">
                 {projects?.map((project) => (
                     <div className="mx-auto my-6" key={project.id}>
-                        <div className='font-medium text-lg mb-1'>{project.title}</div>
+                        <div className="font-medium text-lg mb-1">
+                            {project.title}
+                        </div>
                         <img
-                            src={project.imageUrl}
-                            alt={project.url}
+                            src={project.thumbnail.url}
+                            alt={project.thumbnail.explanation}
                             className="object-cover object-center"
                             style={{ width: 500, height: 300 }}
                         />
