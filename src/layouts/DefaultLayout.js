eca45dafdc580d03b25668f3374a589e92a404c0
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-07-07 00:24:49 +0300

    fix navbar top, add bg white, add mt-16 to main  in default layout

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -6,7 +6,7 @@ export default function DefaultLayout({ children }) {
     return (
         <>
             <Navbar />
-            <main>{children}</main>
+            <main className="mt-16">{children}</main>
             <Footer />
         </>
     )
