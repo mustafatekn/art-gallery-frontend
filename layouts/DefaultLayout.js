b2378c79d3f716dcd6668293282c11d866490693
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-07-09 13:41:04 +0300

    create DefaultLayout, Footer and Navbar

---
+++ layouts/DefaultLayout.js
@@ -0,0 +1,12 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+export default function DefaultLayout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main className="my-20">{children}</main>
+      <Footer />
+    </>
+  );
+}
