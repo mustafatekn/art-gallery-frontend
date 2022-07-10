3b8d306389825f75fcd5a119f4ffef0db9afc8b3
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-07-10 11:50:18 +0300

    add location data for projects

--- pages/projects/[url].js
+++ pages/projects/[url].js
@@ -14,19 +14,31 @@ export default function ProjectDetails({ project }) {
             src={project.thumbnail.url}
             alt={project.thumbnail.explanation}
             className="object-fill w-full"
-            style={{maxHeight:700}}
+            style={{ maxHeight: 700 }}
           />
         </div>
       </div>
-      <div className="grid-cols-12 gap-12 mt-12 lg:grid mx-12">
-        {project?.images?.map((img) => (
-          <div className="flex-col col-span-4 lg:flex">
+      <div className="container w-full mx-auto text-center my-10">
+        <h2 className="font-extrabold text-3xl ">{project.title}</h2>
+        <h6 className="font-bold text-2xl">{project.location}</h6>
+        <p className="font-semibold text-xl">{project.text}</p>
+      </div>
+      <div className="grid-cols-12 gap-12 lg:grid mx-12">
+        {project?.images?.map((img, index) => (
+          <div
+            className={
+              index > 2
+                ? "flex-col col-span-4 lg:flex my-12"
+                : "flex-col col-span-4 lg:flex mb-12"
+            }
+            key={img.url}
+          >
             <div className="flex flex-col gap-12">
               <img
                 src={img.url}
                 className="object-center object-cover w-full h-full"
                 alt={img.explanation}
-                style={{minHeight:500}}
+                style={{ minHeight: 500 }}
               />
             </div>
           </div>
