32ff7fd09ed5688b108af3d63dbbd22641cb13d6
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-07-12 15:17:35 +0300

    improve ui on 404 page

--- pages/404.js
+++ pages/404.js
@@ -8,9 +8,18 @@ export default function NotFound() {
       <Head>
         <title>Rixusart | Not Found</title>
       </Head>
-      <div className="h-screen flex flex-col items-center">
-        <div className="py-36">
-          <Image src="/assets/images/logo.png" width={270} height={36} />
+      <div className="h-screen flex flex-col items-center space-y-28 py-20">
+        <Image src="/assets/images/logo.png" width={270} height={36} />
+        <div className="mt-10">
+          <div className="w-96 h-96 border-4 border-zinc-900 rounded-full items-center justify-center flex">
+            <div className="w-80 h-80 rounded-full flex flex-col justify-center items-center space-y-5 italic">
+              <div className="text-8xl">404</div>
+              <div className="text-6xl">Not Found</div>
+            </div>
+          </div>
+        </div>
+        <div className="text-3xl mt-10">
+          The page you're looking for is not found.
         </div>
       </div>
     </>
