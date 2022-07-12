84cc63c2ba93d4f99cb5fdc04511625004118c20
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-07-12 15:06:54 +0300

    add logo to not found page

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+import DefaultLayout from "../layouts/DefaultLayout";
+
 export default function NotFound() {
   return (
-    <div>404</div>
-  )
+    <>
+      <Head>
+        <title>Rixusart | Not Found</title>
+      </Head>
+      <div className="h-screen flex flex-col items-center">
+        <div className="py-36">
+          <Image src="/assets/images/logo.png" width={270} height={36} />
+        </div>
+      </div>
+    </>
+  );
 }
