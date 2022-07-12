29e330eb3b18b93cf06c8af4321d452733220586
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-07-12 18:05:28 +0300

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import DefaultLayout from "../layouts/DefaultLayout";
+
+export default function Contact() {
+  return (
+    <DefaultLayout>
+      <div className="container mx-auto">contact</div>
+    </DefaultLayout>
+  );
+}
