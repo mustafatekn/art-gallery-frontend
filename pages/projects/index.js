5729e382f19a7d165d709dab602a3cbd51f6ad28
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-07-14 00:39:05 +0300

    create grid system for projects

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -10,27 +10,29 @@ export default function Projects({ projects }) {
       <Head>
         <title>Projects | Rixusart</title>
       </Head>
-      <div className="container flex flex-col mx-auto text-center">
-        {projects?.map((project) => (
-          <div className="mx-auto my-6" key={project.id}>
-            <div className="font-medium text-lg mb-1">{project.title}</div>
-            <div className="px-2 md:px-0">
-              <Link href={`/projects/${project.url}`}>
-                <a>
-                  <Image
-                    loader={() => project.thumbnail.url}
-                    src={project.thumbnail.url}
-                    alt={project.thumbnail.explanation}
-                    className="object-cover object-center hover:contrast-75"
-                    width={700}
-                    height={400}
-                    unoptimized
-                  />
-                </a>
-              </Link>
+      <div className="container flex flex-col mx-auto">
+        <div className="grid grid-cols-1  lg:grid-cols-2 gap-12">
+          {projects?.map((project) => (
+            <div className="mx-auto my-6" key={project.id}>
+              <div className="font-medium text-lg mb-1">{project.title}</div>
+              <div className="px-2 md:px-0">
+                <Link href={`/projects/${project.url}`}>
+                  <a>
+                    <Image
+                      loader={() => project.thumbnail.url}
+                      src={project.thumbnail.url}
+                      alt={project.thumbnail.explanation}
+                      className="object-cover object-center hover:contrast-75"
+                      width={700}
+                      height={400}
+                      unoptimized
+                    />
+                  </a>
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </DefaultLayout>
   );
