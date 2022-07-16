7f093e9e7435e0f5ef133e1e1553cda7c8dffb85
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-07-16 12:51:21 +0300

    remove swiper scrollbar import

--- pages/projects/[url].js
+++ pages/projects/[url].js
@@ -7,7 +7,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
 
 export default function ProjectDetails({ project }) {
   const [show, setShow] = useState(false);
