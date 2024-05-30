25e97a6d411f2abcd213393d47a8a8d67ff170f9
Mustafa Osman Tekin <57020656+mustafatekn@users.noreply.github.com>
2024-05-30 23:25:05 +0300

    replace unknowned image domains with cloudinary domain

--- next.config.js
+++ next.config.js
@@ -3,13 +3,7 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      "helpx.adobe.com",
-      "www.thoughtco.com",
-      "static.remove.bg",
-      "www.pixsy.com",
-      "cdn.pixabay.com",
-    ],
+    domains: ["res.cloudinary.com"],
   },
   env: {
     SERVER_URL: "https://art-gallery-backend.vercel.app"
